3228167acbcb791e51ac42340a0be1e9d0a9f6a0
kasif <kasif@codeaurora.org>
2013-09-25 15:50:30 +0530

    Clear notifications for Deleted items from Download list
    Issue: Notifications for deleted items from the Download
    list are not getting cleared and thus tapping on such
    notifications causes crash (Illegal Argument Exception)
    
    Fix: clear notifications for deleted items by adding them
    in the stale list
    
    Change-Id: I7a27d19fff994876eee07d4c4d63b2808cc516d3
    CRs-Fixed: 536562

--- src/com/android/providers/downloads/DownloadService.java
+++ src/com/android/providers/downloads/DownloadService.java
@@ -323,6 +323,7 @@ public class DownloadService extends Service {
 
                     deleteFileIfExists(info.mFileName);
                     resolver.delete(info.getAllDownloadsUri(), null, null);
+                    staleIds.add(info.mId);
 
                 } else {
                     // Kick off download task if ready
