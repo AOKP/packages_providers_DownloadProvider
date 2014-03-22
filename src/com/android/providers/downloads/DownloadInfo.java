f2625876fa88d42d24df05c02166caa8cd4d5918
Pawel Kozak <pawel.kozak@sonymobile.com>
2014-03-22 20:40:42 +0530

    DownloadProvider: Remove download notification after preview
    Removes download notification from the notification bar after
    previewing it. There should be an additional check for download
    shown in the notifications after completion to prevent from
    situation when ContentObserver is unregistered and the onChange
    is never called
    
    Change-Id: I339c08152caf53342cf17e34c46e187fdad5a4c0

--- src/com/android/providers/downloads/DownloadInfo.java
+++ src/com/android/providers/downloads/DownloadInfo.java
@@ -353,7 +353,8 @@ public class DownloadInfo {
         if (!Downloads.Impl.isStatusCompleted(mStatus)) {
             return false;
         }
-        if (mVisibility == Downloads.Impl.VISIBILITY_VISIBLE_NOTIFY_COMPLETED) {
+        if (mVisibility == Downloads.Impl.VISIBILITY_VISIBLE_NOTIFY_COMPLETED ||
+                mVisibility == DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_ONLY_COMPLETION) {
             return true;
         }
         return false;
